996f00450694c2d4265bba87e7089224ac49b3d7
MudassarA10 <123820684+MudassarA10@users.noreply.github.com>
2024-02-06 19:19:52 +0500

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://MudassarA10.github.io/TODO-LIST",
+  base: "https://MudassarA10.github.io/Todo-list-Project",
 });
